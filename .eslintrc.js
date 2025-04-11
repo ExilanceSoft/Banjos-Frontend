5fa5872d7dc2a20d189aa32d7851d2ed83bf0b32
ExilanceSoft <exilancesoft@gmail.com>
2025-04-11 15:43:56 +0530

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,25 @@
 module.exports = {
-
   parserOptions: {
-    ecmaVersion: 2020, 
-    sourceType: 'module', 
+    ecmaVersion: 2020, // Allows for parsing modern ECMAScript
+    sourceType: 'module', // Allows for using imports
     ecmaFeatures: {
-      jsx: true, 
-
-  // parser: '@typescript-eslint/parser', // Specifies the ESLint parser
-  parserOptions: {
-    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
-    sourceType: 'module', // Allows for the use of imports
-    ecmaFeatures: {
-      jsx: true, // Allows for the parsing of JSX
+      jsx: true, // Allows JSX parsing
     },
   },
   settings: {
     react: {
-      version: 'detect', 
-
-      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+      version: 'detect', // Automatically detect React version
     },
   },
   extends: [
     'react-app',
     'react-app/jest',
     'plugin:react/recommended',
-    'plugin:prettier/recommended', 
+    'plugin:prettier/recommended', // Always keep this last
   ],
   plugins: ['react', 'react-hooks'],
   rules: {
-    'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
-    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-  ],
-  plugins: ['react', 'react-hooks'],
-  rules: {
-    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
     'react/display-name': 'off',
-   "prettier/prettier": "off"
+    'prettier/prettier': 'off',
   },
-}
+};
